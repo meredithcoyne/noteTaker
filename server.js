03cd19a0851987f2cd335f8bd551cc85ca646e75
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-14 22:25:03 -0500

    create routes files and started server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+// require express, path, ./routes/apiRoutes
+const express = require(`express`);
+const path = require(`path`);
+const api = require(`./routes/apiRoutes`);
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
