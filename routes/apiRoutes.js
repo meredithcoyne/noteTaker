7bbeed68bc48051d55e3521cf16fcca6015902c6
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-12-04 16:20:02 -0500

    added the GET api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,12 @@ const store = require('../db/store');
 
 
 //GET "api/notes" reads db.json 
-
+router.get('/notes' , (req,res) => {
+    store.getNotes().then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
 
 //POST /api/notes receives new notes and adds to db.json
 
